00e0d4fa833579d33eae597d9ca5a4138bf251cb
mdpriselac <mdpriselac@gmail.com>
2024-07-12 14:43:14 -0700

    fixed fresh dashboard

--- pages/fresh_dashboard_page.py
+++ pages/fresh_dashboard_page.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import pandas as pd
 from datetime import datetime, timedelta
-from pages.modules.df_location import df_csv_path
+from pages.individual_coffee_page import load_full_dataset
 
-def fresh_dashboard(csv_path):
-    full_df = pd.read_csv(csv_path,index_col=0)
+def fresh_dashboard():
+    full_df = load_full_dataset()
     country_proportion = full_df['country_final'].value_counts(normalize=True)
     month_ago = datetime.today() - timedelta(days=30)
     full_df['first_date_observed_dto'] = pd.to_datetime(full_df['first_date_observed'])
@@ -23,4 +23,4 @@ def fresh_dashboard(csv_path):
     st.markdown("The Fresh metric is an attempt to estimate whether a crop of coffees is currently arriving.\n We're combining three pieces of information: \n- The proportion of all coffees in the data set that come from a country. Call this the country's baseline proportion. \n- The proportion of coffees arriving in the last 30 days that come from a country. Call the this the country's recent proportion. \n- The raw count of coffees arriving in the last 30 days from a country.\n\nWe're combining these pieces of information by multiplying the country's raw count of coffees arriving in the last 30 days by the raio of the country's recent proportion to its baseline proportion. So, when a country's Fresh metric coffees are arriving from that country at an unusually high rate. If it's very low, then though a new coffee or two as arrived, it's acutlly a slower arrival proportion than the country normallly makes up.")
     st.dataframe(fresh_szn.sort_values(ascending=False))
     
-fresh_dashboard(df_csv_path)
+fresh_dashboard()
