d43e96691c07cc4a3fbf23136cafd2a044cb5f46
mdpriselac <mdpriselac@gmail.com>
2024-07-12 14:46:35 -0700

    fixed some usa to canada language

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-st.set_page_config(page_title='Green Coffee in the USA', page_icon='☕️', layout='wide', initial_sidebar_state='expanded')
+st.set_page_config(page_title='Green Coffee in Canada', page_icon='☕️', layout='wide', initial_sidebar_state='expanded')
 
-pages = [st.Page('pages/about_page.py',title='About the Home Greens Project',default=True), 
+pages = [st.Page('pages/about_page.py',title='About the Home Greens Project Canada',default=True), 
          st.Page('pages/full_data_set_page.py',title='All Green Coffees'), 
          st.Page('pages/individual_coffee_page.py',title='Individual Coffee Information'), 
          st.Page('pages/fresh_dashboard_page.py',title='Fresh Arrival Dashboard')]
